3a4ef6edc24ef337f181bc700002d7930e26b3f4
Seung Bin . Ko <kosb15@naver.com>
2020-01-31 07:07:29 +0900

    19장. 테이블 차리기

--- xUnit/test.py
+++ xUnit/test.py
@@ -1,6 +1,6 @@
 
 # (Done) 테스트 메서드 호출하기
-# () 먼저 setUp 호출하기
+# (Done) 먼저 setUp 호출하기
 # () 나중에 tearDown 호출하기
 # () 테스트 메서드가 실패하더라도 tearDown 호출하기
 # () 여러 개의 테스트 실행하기
@@ -10,25 +10,39 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method= getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun= None
         TestCase.__init__(self, name)
 
     def testMethod(self):
         self.wasRun= 1
 
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp= 1
+
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test= WasRun("testMethod")
+
     def testRunning(self):
-        test= WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
 
 TestCaseTest("testRunning").run()
+
+TestCaseTest("testSetUp").run()
 
 
