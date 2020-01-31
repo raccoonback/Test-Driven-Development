395c6079cdd7b802e819224a12c6ee16a5181652
Seung Bin . Ko <kosb15@naver.com>
2020-01-31 17:16:15 +0900

    20장. 뒷정리하기

--- xUnit/test.py
+++ xUnit/test.py
@@ -1,10 +1,11 @@
 
 # (Done) 테스트 메서드 호출하기
 # (Done) 먼저 setUp 호출하기
-# () 나중에 tearDown 호출하기
+# (Done) 나중에 tearDown 호출하기
 # () 테스트 메서드가 실패하더라도 tearDown 호출하기
 # () 여러 개의 테스트 실행하기
 # () 수집된 결과를 출력하기
+# (Done) WasRun에 로그 문자열 남기기
 
 class TestCase:
     def __init__(self, name):
@@ -17,32 +18,29 @@ class TestCase:
         self.setUp()
         method= getattr(self, self.name)
         method()
+        self.tearDown()
+
+    def tearDown(self):
+        pass
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        TestCase.__init__(self, name)
+    def setUp(self):
+        self.wasRun = None
+        self.log= "setUp "
 
     def testMethod(self):
         self.wasRun= 1
+        self.log= self.log + "testMethod "
 
-    def setUp(self):
-        self.wasRun = None
-        self.wasSetUp= 1
+    def tearDown(self):
+        self.log= self.log + "tearDown "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test= WasRun("testMethod")
+    def testTemplateMethod(self):
+        test= WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
-
-TestCaseTest("testRunning").run()
-
-TestCaseTest("testSetUp").run()
+TestCaseTest("testTemplateMethod").run()
 
 
